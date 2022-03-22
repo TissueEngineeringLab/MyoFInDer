7bfa0e672a3475f9f000a267b704d2ff258709af
WeisLeDocto <antoine.weisrock@gmail.com>
2022-03-22 17:22:43 +0100

    [ref] Improved the compile.bat file to automize the generation of the MSI installer

--- Executable_files/compile.bat
+++ Executable_files/compile.bat
@@ -4,7 +4,12 @@ where /q pyinstaller
 if errorlevel 1 (
     echo You need to install the python module pyinstaller for compiling cellen-tellen !
 ) else (
-    :: To be tested and completed
     cd ..
-    pyinstaller run.py --name cellen-tellen --paths .\venv\Lib\site-packages\ --hidden-import sklearn.utils._typedefs --hidden-import sklearn.neighbors._partition_nodes --add-data ".\cellen_tellen\app_images\;app_images"
+    pyinstaller run.py --noconfirm --name cellen-tellen --paths .\venv\Lib\site-packages\ --hidden-import sklearn.utils._typedefs --hidden-import sklearn.neighbors._partition_nodes --add-data ".\cellen_tellen\app_images\;app_images"
+    if exist .\dist\ (
+        mkdir .\dist\Projects
+        "C:\Program Files (x86)\Caphyon\Advanced Installer 19.3\bin\x86\AdvancedInstaller.com" /build .\Executable_files\Cellen-Tellen.aip
+    ) else (
+        echo Something went wrong, dist folder not found !
+    )
 )
