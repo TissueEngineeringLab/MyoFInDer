a814db7eb113e64e3a02ec360b944d084a587fec
WeisLeDocto <antoine.weisrock@gmail.com>
2023-03-01 17:09:24 +0100

    [ref] Changed the run.py file so that the console doesn't close at the end of the program in case an error occurs

--- run.py
+++ run.py
@@ -3,7 +3,27 @@
 """Executable file for running the application"""
 
 from cellen_tellen import Main_window
+import logging
+from sys import stdout
 
 if __name__ == "__main__":
 
-    Main_window()
+    logger = logging.getLogger("Cellen-Tellen")
+    logger.setLevel(logging.INFO)
+
+    handler = logging.StreamHandler(stream=stdout)
+    handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)-8s '
+                                  '%(message)s')
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
+    try:
+        Main_window()
+    except (Exception,) as exc:
+        logger.exception("Cellen-Tellen encountered an error while running !",
+                         exc_info=exc)
+        print("\n\nPress ENTER to exit ...")
+        input()
