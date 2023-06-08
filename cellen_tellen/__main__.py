d21baf1308a6c653b66d0dc6f8a8e8750cfa9b67
Antoine Weisrock <antoine.weisrock@gmail.com>
2023-06-08 10:34:53 +0200

    [upd] Added command line arguments to the module
    One argument signals the module that it was started from an application
    The other indicates whether log messages should be recorded and displayed

--- cellen_tellen/__main__.py
+++ cellen_tellen/__main__.py
@@ -6,9 +6,28 @@ from . import Main_window
 import logging
 from sys import stdout
 from pathlib import Path
+import argparse
 
 
 if __name__ == "__main__":
+
+    # Parser for parsing the command line arguments of the script
+    parser = argparse.ArgumentParser(
+        description="Starts Cellen-Tellen, with optional flags for specifying "
+                    "whether to record log messages and whether the module was"
+                    " started by the application or independently.")
+    parser.add_argument('-a', '--app', action='store_true',
+                        help="If provided, indicates the module that it was "
+                             "started from an application. It consequently "
+                             "enables or disables some features.")
+    parser.add_argument('-l', '--log', action='store_true',
+                        help="If provided, the log messages will be displayed "
+                             "and recorded. Otherwise, they will be ignored.")
+    args = parser.parse_args()
+
+    # Retrieving the command-line arguments
+    from_app = args.app
+    log = args.log
 
     base_path = Path(__file__).parent
     if Path(__file__).name.endswith(".pyc"):
