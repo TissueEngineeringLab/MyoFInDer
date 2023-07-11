6c68456be8296801d96a5079e81d6fc965eaf825
WeisLeDocto <antoine.weisrock@gmail.com>
2023-07-11 16:50:20 +0200

    [del] Removed the now useless files for building the previous windows installers

--- Executable_files/cellen-tellen.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# File for running the cellen-tellen executable in Linux
-# It is meant to be copy-pasted in the dist folder generated when compiling
-
-if [ -e ./cellen-tellen/cellen-tellen ]; then
-  ./cellen-tellen/cellen-tellen
-else
-  echo "Couldn't find the cellen-tellen executable, aborting !"
-fi
--- Executable_files/compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# File for compiling cellen-tellen into an executable file in Linux
-
-if type "pyinstaller" > /dev/null; then
-  cd ..
-  pyinstaller run.py --name cellen-tellen --paths ./venv/lib/python3.8/site-packages/ --hidden-import sklearn.utils._typedefs --hidden-import sklearn.neighbors._partition_nodes --hidden-import PIL._tkinter_finder --add-data "./cellen_tellen/app_images/:app_images"
-else
-  echo "You need to install the python module pyinstaller for compiling cellen-tellen !"
-fi
