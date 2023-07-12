5b3e41acaae89523917bd21a2dc75e0d6b1a234b
Antoine Weisrock <antoine.weisrock@gmail.com>
2023-07-12 15:35:09 +0200

    [ref] Added classifiers and URLs to the setup.py file

--- setup.py
+++ setup.py
@@ -29,12 +29,27 @@ setup(
   """,
   keywords='segmentation,fusion index,automation,muscle culture',
   license='GPL V3',
-  classifiers=list(),
+  classifiers=['Development Status :: 4 - Beta',
+               'Intended Audience :: Science/Research',
+               'License :: OSI Approved :: GNU General Public License v3 '
+               '(GPLv3)',
+               'Natural Language :: English',
+               'Operating System :: OS Independent',
+               'Programming Language :: Python :: 3.7',
+               'Programming Language :: Python :: 3.8',
+               'Programming Language :: Python :: 3.9',
+               'Programming Language :: Python :: 3.10',
+               'Topic :: Scientific/Engineering :: Artificial Intelligence',
+               'Topic :: Scientific/Engineering :: Bio-Informatics',
+               'Topic :: Scientific/Engineering :: Image Recognition'
+               ],
 
   # URLs of the project
   url='https://github.com/WeisLeDocto/Cellen-Tellen',
   download_url='https://pypi.org/project/cellen_tellen/#files',
-  project_urls=dict(),
+  project_urls={
+    'Documentation': 'https://weisledocto.github.io/Cellen-Tellen/',
+    'Source': 'https://github.com/WeisLeDocto/Cellen-Tellen'},
 
   # Information on the author
   author='Antoine Weisrock',
