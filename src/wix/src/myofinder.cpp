837e5d1868a5212e8a8c204ff291319fd04d4da1
Antoine Weisrock <antoine.weisrock@gmail.com>
2024-03-14 23:00:54 +0100

    build: read install dir from registry and pass it as cmd-line arg

--- src/wix/src/myofinder.cpp
+++ src/wix/src/myofinder.cpp
@@ -1,5 +1,7 @@
 #include <shlwapi.h>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 #include "run_cmd.cpp"
 #include "read_register.cpp"
 
@@ -10,34 +12,48 @@ mode, and finally launches MyoFInDer.
 */
 int main(int argc, char* argv[]){
 
+    // Putting the arguments into a vector for convenience
+    std::vector<std::string> args(argv, argv+argc);
+
     // Retrieving the path to the Python executable of MyoFInDer
     std::string python_exe_path = read_reg(HKEY_CURRENT_USER, "SOFTWARE\\MyoFInDer", "MyoFInDerPythonPath");
-    std::cout << "Python executable path: \"" + python_exe_path + "\"\n\n";
+    std::cout << "Python executable path: \"" + python_exe_path + "\"\n";
+
+    // Retrieving the path to the installation directory of MyoFInDer
+    std::string install_dir = read_reg(HKEY_CURRENT_USER, "SOFTWARE\\MyoFInDer", "MyoFInDerInstallDir");
+    std::cout << "Installation directory: \"" + install_dir + "\"\n\n";
+
+    // String containing the application folder argument to pass to MyoFInDer
+    std::string app_dir_arg = " --app-folder \"" + install_dir + "\"";
 
     // Checking if the myofinder module should be started in test mode, and
     // setting the suffix to add to the Python command to execute
-    std::string test;
+    std::string test_arg;
+    int test;
     std::cout << "Checking if MyoFInDer should run in test mode.\n";
-    if (argc > 1 && strcmp(argv[1], "-t") == 0){
+    if (std::find(args.begin(), args.end(), "--test") != args.end() ||
+        std::find(args.begin(), args.end(), "-t") != args.end()){
         std::cout << "Running MyoFInDer in test mode.\n\n";
-        test = "-t";
+        test = 1;
+        test_arg = " --test";
     }
     else {
         std::cout << "Running MyoFInDer in regular mode.\n\n";
-        test = "";
+        test = 0;
+        test_arg = "";
     }
 
     // Starting the myofinder module using the dedicated Python interpreter
-    std::string myofinder_cmd = "\"" + python_exe_path + "\"" + " -m myofinder " + test;
+    std::string myofinder_cmd = "\"" + python_exe_path + "\"" + " -m myofinder" + test_arg + app_dir_arg;
     std::cout << "Command to run: " + myofinder_cmd + "\n";
-    if (argc > 1 && strcmp(argv[1], "-t") == 0){
+    if (test){
         std::cout << "Starting MyoFInDer in test mode.\n\n";
     }
     else {
         std::cout << "Starting MyoFInDer.\n\n";
     }
     // No new console in test mode, so that GitHub Actions can display the log
-    run_cmd((char*) myofinder_cmd.data(), (argc > 1 && strcmp(argv[1], "-t") == 0) ? 0 : CREATE_NEW_CONSOLE);
+    run_cmd((char*) myofinder_cmd.data(), test ? 0 : CREATE_NEW_CONSOLE);
 
     return 0;
 }
