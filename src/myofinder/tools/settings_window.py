bdad5dcce3efb2b7691c0cc4cdfdd66d996dd736
Antoine Weisrock <antoine.weisrock@gmail.com>
2024-06-04 23:52:32 +0200

    feat: make every graphical element of the settings window an attribute
    Previously, no reference to the graphical elements were kept.
    These references are needed for automating the unit tests of the settings window.

--- src/myofinder/tools/settings_window.py
+++ src/myofinder/tools/settings_window.py
@@ -43,156 +43,186 @@ class SettingsWindow(Toplevel):
         """Creates the buttons and sliders, places them and displays them."""
 
         # General layout
-        frame = ttk.Frame(self, padding="20 20 20 20")
-        frame.grid(sticky='NESW')
+        self._frame = ttk.Frame(self, padding="20 20 20 20")
+        self._frame.grid(sticky='NESW')
 
         # Buttons for selecting the nuclei channel
-        ttk.Label(frame, text="Nuclei Channel :").grid(column=0, row=0,
-                                                       sticky='NE',
-                                                       padx=(0, 10))
-        nuclei_colour_r1 = ttk.Radiobutton(
-            frame, text="Blue", variable=self._settings.nuclei_colour,
+        self._nuclei_colour_label = ttk.Label(self._frame,
+                                              text="Nuclei Channel :")
+        self._nuclei_colour_label.grid(column=0, row=0, sticky='NE',
+                                       padx=(0, 10))
+        self._nuclei_colour_r1 = ttk.Radiobutton(
+            self._frame, text="Blue", variable=self._settings.nuclei_colour,
             value="blue")
-        nuclei_colour_r1.grid(column=1, row=0, sticky='NW')
-        nuclei_colour_r2 = ttk.Radiobutton(
-            frame, text="Green", variable=self._settings.nuclei_colour,
+        self._nuclei_colour_r1.grid(column=1, row=0, sticky='NW')
+        self._nuclei_colour_r2 = ttk.Radiobutton(
+            self._frame, text="Green", variable=self._settings.nuclei_colour,
             value="green")
-        nuclei_colour_r2.grid(column=1, row=1, sticky='NW')
-        nuclei_colour_r3 = ttk.Radiobutton(
-            frame, text="Red", variable=self._settings.nuclei_colour,
+        self._nuclei_colour_r2.grid(column=1, row=1, sticky='NW')
+        self._nuclei_colour_r3 = ttk.Radiobutton(
+            self._frame, text="Red", variable=self._settings.nuclei_colour,
             value="red")
-        nuclei_colour_r3.grid(column=1, row=2, sticky='NW')
+        self._nuclei_colour_r3.grid(column=1, row=2, sticky='NW')
 
         # Buttons for selecting the fibers channel
-        ttk.Label(frame, text="Fiber Channel :").grid(
-            column=0, row=3, sticky='NE', pady=(10, 0), padx=(0, 10))
-        fiber_colour_r1 = ttk.Radiobutton(
-            frame, text="Blue", variable=self._settings.fiber_colour,
+        self._fiber_colour_label = ttk.Label(self._frame,
+                                             text="Fiber Channel :")
+        self._fiber_colour_label.grid(column=0, row=3, sticky='NE',
+                                      pady=(10, 0), padx=(0, 10))
+        self._fiber_colour_r1 = ttk.Radiobutton(
+            self._frame, text="Blue", variable=self._settings.fiber_colour,
             value="blue")
-        fiber_colour_r1.grid(column=1, row=3, sticky='NW', pady=(10, 0))
-        fiber_colour_r2 = ttk.Radiobutton(
-            frame, text="Green", variable=self._settings.fiber_colour,
+        self._fiber_colour_r1.grid(column=1, row=3, sticky='NW', pady=(10, 0))
+        self._fiber_colour_r2 = ttk.Radiobutton(
+            self._frame, text="Green", variable=self._settings.fiber_colour,
             value="green")
-        fiber_colour_r2.grid(column=1, row=4, sticky='NW')
-        fiber_colour_r3 = ttk.Radiobutton(
-            frame, text="Red", variable=self._settings.fiber_colour,
+        self._fiber_colour_r2.grid(column=1, row=4, sticky='NW')
+        self._fiber_colour_r3 = ttk.Radiobutton(
+            self._frame, text="Red", variable=self._settings.fiber_colour,
             value="red")
-        fiber_colour_r3.grid(column=1, row=5, sticky='NW')
+        self._fiber_colour_r3.grid(column=1, row=5, sticky='NW')
 
         # Buttons to chose whether to save the overlay images or not
-        ttk.Label(frame, text='Save Images with Overlay :').grid(
-            column=0, row=11, sticky='NE', pady=(10, 0), padx=(0, 10))
-        ttk.Radiobutton(
-            frame, text="On", variable=self._settings.save_overlay,
-            value=1).grid(column=1, row=11, sticky='NW', pady=(10, 0))
-        ttk.Radiobutton(
-            frame, text="Off", variable=self._settings.save_overlay,
-            value=0).grid(column=1, row=12, sticky='NW')
+        self._overlay_label = ttk.Label(self._frame,
+                                        text='Save Images with Overlay :')
+        self._overlay_label.grid(column=0, row=11, sticky='NE', pady=(10, 0),
+                                 padx=(0, 10))
+        self._overlay_on_button = ttk.Radiobutton(
+            self._frame, text="On", variable=self._settings.save_overlay,
+            value=1)
+        self._overlay_on_button.grid(column=1, row=11, sticky='NW',
+                                     pady=(10, 0))
+        self._overlay_off_button = ttk.Radiobutton(
+            self._frame, text="Off", variable=self._settings.save_overlay,
+            value=0)
+        self._overlay_off_button.grid(column=1, row=12, sticky='NW')
 
         # Slider to adjust the minimum intensity for fiber detection
-        ttk.Label(frame, text='Minimum fiber intensity :').grid(
-            column=0, row=13, sticky='E', pady=(10, 0), padx=(0, 10))
+        self._min_fiber_int_label = ttk.Label(self._frame,
+                                              text='Minimum fiber intensity :')
+        self._min_fiber_int_label.grid(column=0, row=13, sticky='E',
+                                       pady=(10, 0), padx=(0, 10))
 
-        minimum_fiber_intensity_slider_frame = ttk.Frame(frame)
+        self._minimum_fiber_intensity_slider_frame = ttk.Frame(self._frame)
 
-        ttk.Label(minimum_fiber_intensity_slider_frame,
-                  textvariable=self._settings.minimum_fiber_intensity,
-                  width=3). \
-            pack(side='left', anchor='w', fill='none', expand=False,
-                 padx=(0, 20))
+        self._min_fib_int_slide_val_label = ttk.Label(
+            self._minimum_fiber_intensity_slider_frame,
+            textvariable=self._settings.minimum_fiber_intensity,
+            width=3)
+        self._min_fib_int_slide_val_label.pack(
+            side='left', anchor='w', fill='none', expand=False, padx=(0, 20))
 
-        Scale(minimum_fiber_intensity_slider_frame, from_=0, to=100,
-              orient="horizontal",
-              variable=self._settings.minimum_fiber_intensity, showvalue=False,
-              length=150, tickinterval=20). \
-            pack(side='left', anchor='w', fill='none', expand=False)
+        self._min_fib_int_slider = Scale(
+            self._minimum_fiber_intensity_slider_frame, from_=0, to=100,
+            orient="horizontal",
+            variable=self._settings.minimum_fiber_intensity, showvalue=False,
+            length=150, tickinterval=20)
 
-        minimum_fiber_intensity_slider_frame.grid(column=1, row=13,
-                                                  sticky='NW', pady=(10, 0))
+        self._min_fib_int_slider.pack(side='left', anchor='w', fill='none',
+                                      expand=False)
+
+        self._minimum_fiber_intensity_slider_frame.grid(
+            column=1, row=13, sticky='NW', pady=(10, 0))
 
         # Slider to adjust the maximum intensity for fiber detection
-        ttk.Label(frame, text='Maximum fiber intensity :').grid(
-            column=0, row=14, sticky='E', pady=(10, 0), padx=(0, 10))
+        self._max_fiber_int_label = ttk.Label(self._frame,
+                                              text='Maximum fiber intensity :')
+        self._max_fiber_int_label.grid(column=0, row=14, sticky='E',
+                                       pady=(10, 0), padx=(0, 10))
 
-        maximum_fiber_intensity_slider_frame = ttk.Frame(frame)
+        self._maximum_fiber_intensity_slider_frame = ttk.Frame(self._frame)
 
-        ttk.Label(maximum_fiber_intensity_slider_frame,
-                  textvariable=self._settings.maximum_fiber_intensity,
-                  width=3). \
-            pack(side='left', anchor='w', fill='none', expand=False,
-                 padx=(0, 20))
+        self._max_fib_int_slide_val_label = ttk.Label(
+            self._maximum_fiber_intensity_slider_frame,
+            textvariable=self._settings.maximum_fiber_intensity, width=3)
+        self._max_fib_int_slide_val_label.pack(
+            side='left', anchor='w', fill='none', expand=False, padx=(0, 20))
 
-        Scale(maximum_fiber_intensity_slider_frame, from_=155, to=255,
-              orient="horizontal",
-              variable=self._settings.maximum_fiber_intensity, showvalue=False,
-              length=150, tickinterval=25). \
-            pack(side='left', anchor='w', fill='none', expand=False)
+        self._max_fib_int_slider = Scale(
+            self._maximum_fiber_intensity_slider_frame, from_=155, to=255,
+            orient="horizontal",
+            variable=self._settings.maximum_fiber_intensity, showvalue=False,
+            length=150, tickinterval=25)
+        self._max_fib_int_slider.pack(side='left', anchor='w', fill='none',
+                                      expand=False)
 
-        maximum_fiber_intensity_slider_frame.grid(column=1, row=14,
-                                                  sticky='NW', pady=(10, 0))
+        self._maximum_fiber_intensity_slider_frame.grid(
+            column=1, row=14, sticky='NW', pady=(10, 0))
 
         # Slider to adjust the minimum intensity for nuclei detection
-        ttk.Label(frame, text='Minimum nucleus intensity :').grid(
-            column=0, row=15, sticky='E', pady=(10, 0), padx=(0, 10))
+        self._min_nucleus_int_label = ttk.Label(
+            self._frame, text='Minimum nucleus intensity :')
+        self._min_nucleus_int_label.grid(column=0, row=15, sticky='E',
+                                         pady=(10, 0), padx=(0, 10))
 
-        minimum_nuclei_intensity_slider_frame = ttk.Frame(frame)
+        self._minimum_nuclei_intensity_slider_frame = ttk.Frame(self._frame)
 
-        ttk.Label(minimum_nuclei_intensity_slider_frame,
-                  textvariable=self._settings.minimum_nucleus_intensity,
-                  width=3). \
-            pack(side='left', anchor='w', fill='none', expand=False,
-                 padx=(0, 20))
+        self._min_nuc_int_slide_val_label = ttk.Label(
+            self._minimum_nuclei_intensity_slider_frame,
+            textvariable=self._settings.minimum_nucleus_intensity, width=3)
+        self._min_nuc_int_slide_val_label.pack(
+            side='left', anchor='w', fill='none', expand=False, padx=(0, 20))
 
-        Scale(minimum_nuclei_intensity_slider_frame, from_=0, to=100,
-              orient="horizontal",
-              variable=self._settings.minimum_nucleus_intensity,
-              showvalue=False, length=150, tickinterval=20). \
-            pack(side='left', anchor='w', fill='none', expand=False)
+        self._min_nuc_int_slider = Scale(
+            self._minimum_nuclei_intensity_slider_frame, from_=0, to=100,
+            orient="horizontal",
+            variable=self._settings.minimum_nucleus_intensity,
+            showvalue=False, length=150, tickinterval=20)
+        self._min_nuc_int_slider.pack(side='left', anchor='w', fill='none',
+                                      expand=False)
 
-        minimum_nuclei_intensity_slider_frame.grid(column=1, row=15,
-                                                   sticky='NW', pady=(10, 0))
+        self._minimum_nuclei_intensity_slider_frame.grid(
+            column=1, row=15, sticky='NW', pady=(10, 0))
 
         # Slider to adjust the maximum intensity for nuclei detection
-        ttk.Label(frame, text='Maximum nucleus intensity :').grid(
-            column=0, row=16, sticky='E', pady=(10, 0), padx=(0, 10))
+        self._max_nucleus_int_label = ttk.Label(
+            self._frame, text='Maximum nucleus intensity :')
+        self._max_nucleus_int_label.grid(column=0, row=16, sticky='E',
+                                         pady=(10, 0), padx=(0, 10))
 
-        maximum_nuclei_intensity_slider_frame = ttk.Frame(frame)
+        self._maximum_nuclei_intensity_slider_frame = ttk.Frame(self._frame)
 
-        ttk.Label(maximum_nuclei_intensity_slider_frame,
-                  textvariable=self._settings.maximum_nucleus_intensity,
-                  width=3). \
-            pack(side='left', anchor='w', fill='none', expand=False,
-                 padx=(0, 20))
+        self._max_nuc_int_slide_val_label = ttk.Label(
+            self._maximum_nuclei_intensity_slider_frame,
+            textvariable=self._settings.maximum_nucleus_intensity, width=3)
+        self._max_nuc_int_slide_val_label.pack(
+            side='left', anchor='w', fill='none', expand=False, padx=(0, 20))
 
-        Scale(maximum_nuclei_intensity_slider_frame, from_=155, to=255,
-              orient="horizontal",
-              variable=self._settings.maximum_nucleus_intensity,
-              showvalue=False, length=150, tickinterval=25). \
-            pack(side='left', anchor='w', fill='none', expand=False)
+        self._max_nuc_int_slider = Scale(
+            self._maximum_nuclei_intensity_slider_frame, from_=155, to=255,
+            orient="horizontal",
+            variable=self._settings.maximum_nucleus_intensity,
+            showvalue=False, length=150, tickinterval=25)
+        self._max_nuc_int_slider.pack(side='left', anchor='w', fill='none',
+                                      expand=False)
 
-        maximum_nuclei_intensity_slider_frame.grid(column=1, row=16,
-                                                   sticky='NW', pady=(10, 0))
+        self._maximum_nuclei_intensity_slider_frame.grid(
+            column=1, row=16, sticky='NW', pady=(10, 0))
 
         # Slider to adjust the minimum nucleus diameter
-        ttk.Label(frame, text='Minimum nucleus diameter (px) :').grid(
+        self._min_nucleus_diam_label = ttk.Label(
+            self._frame, text='Minimum nucleus diameter (px) :')
+        self._min_nucleus_diam_label.grid(
             column=0, row=17, sticky='E', pady=(10, 0), padx=(0, 10))
 
-        diameter_slider_frame = ttk.Frame(frame)
+        self._diameter_slider_frame = ttk.Frame(self._frame)
 
-        ttk.Label(diameter_slider_frame,
-                  textvariable=self._settings.minimum_nuc_diameter,
-                  width=3). \
-            pack(side='left', anchor='w', fill='none', expand=False,
-                 padx=(0, 20))
+        self._min_nuc_diam_slide_val_label = ttk.Label(
+            self._diameter_slider_frame,
+            textvariable=self._settings.minimum_nuc_diameter, width=3)
+        self._min_nuc_diam_slide_val_label.pack(
+            side='left', anchor='w', fill='none', expand=False, padx=(0, 20))
 
-        Scale(diameter_slider_frame, from_=0, to=100,
-              variable=self._settings.minimum_nuc_diameter,
-              orient="horizontal", length=150, showvalue=False,
-              tickinterval=25). \
-            pack(side='left', anchor='w', fill='none', expand=False)
+        self._min_nuc_diam_slider = Scale(
+            self._diameter_slider_frame, from_=0, to=100,
+            variable=self._settings.minimum_nuc_diameter,
+            orient="horizontal", length=150, showvalue=False,
+            tickinterval=25)
+        self._min_nuc_diam_slider.pack(side='left', anchor='w',
+                                       fill='none', expand=False)
 
-        diameter_slider_frame.grid(column=1, row=17, sticky='NW',
-                                   pady=(10, 0))
+        self._diameter_slider_frame.grid(column=1, row=17, sticky='NW',
+                                         pady=(10, 0))
 
     def _center(self) -> None:
         """Centers the popup window on the currently used monitor."""
