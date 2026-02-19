ade36a6554d24017dd0f58eff923ae02fd3ef591
Antoine Weisrock <antoine.weisrock@gmail.com>
2026-02-19 14:24:14 +0100

    style: optimize readability with early returns

--- src/myofinder/image_canvas.py
+++ src/myofinder/image_canvas.py
@@ -447,18 +447,20 @@ class ImageCanvas(ttk.Frame):
         """
 
         # Do nothing if there's no image displayed
-        if self._image is not None:
+        if self._image is None:
+            return
 
-            # The coordinates of the click on the unscaled image
-            abs_x = self._canvas.canvasx(event.x) / self._img_scale
-            abs_y = self._canvas.canvasy(event.y) / self._img_scale
+        # The coordinates of the click on the unscaled image
+        abs_x = self._canvas.canvasx(event.x) / self._img_scale
+        abs_y = self._canvas.canvasy(event.y) / self._img_scale
 
-            # Do something only if the click is inside the image
-            if abs_x < self._image.width and abs_y < self._image.height:
+        # Do something only if the click is inside the image
+        if abs_x >= self._image.width or abs_y >= self._image.height:
+            return
 
-                # Saving the position of the click
-                self._selection_box.x_start = abs_x
-                self._selection_box.y_start = abs_y
+        # Saving the position of the click
+        self._selection_box.x_start = abs_x
+        self._selection_box.y_start = abs_y
 
     def _motion(self, event: Event) -> None:
         """Binding method for a mouse motion event while either the left or
@@ -473,31 +475,33 @@ class ImageCanvas(ttk.Frame):
 
         # Do nothing if there's no image displayed
         # Also nothing if the selection box wasn't initialized on first click
-        if self._image is not None and self._selection_box.started:
+        if self._image is None or not self._selection_box.started:
+            return
 
-            # The coordinates of the event on the unscaled image
-            abs_x = self._canvas.canvasx(event.x) / self._img_scale
-            abs_y = self._canvas.canvasy(event.y) / self._img_scale
+        # The coordinates of the event on the unscaled image
+        abs_x = self._canvas.canvasx(event.x) / self._img_scale
+        abs_y = self._canvas.canvasy(event.y) / self._img_scale
 
-            # Do something only if the event is inside the image
-            if abs_x < self._image.width and abs_y < self._image.height:
+        # Do something only if the event is inside the image
+        if abs_x >= self._image.width or abs_y >= self._image.height:
+            return
 
-                # Saving the end position of the selection box
-                self._selection_box.x_end = abs_x
-                self._selection_box.y_end = abs_y
+        # Saving the end position of the selection box
+        self._selection_box.x_end = abs_x
+        self._selection_box.y_end = abs_y
 
-                # Drawing the selection box if it is not already displayed
-                if self._selection_box.tk_obj is None:
-                    self._selection_box.tk_obj = self._draw_box()
+        # Drawing the selection box if it is not already displayed
+        if self._selection_box.tk_obj is None:
+            self._selection_box.tk_obj = self._draw_box()
 
-                # Otherwise just updating the displayed selection box
-                else:
-                    self._canvas.coords(
-                        self._selection_box.tk_obj,
-                        (self._selection_box.x_start * self._img_scale,
-                         self._selection_box.y_start * self._img_scale,
-                         self._selection_box.x_end * self._img_scale,
-                         self._selection_box.y_end * self._img_scale))
+        # Otherwise just updating the displayed selection box
+        else:
+            self._canvas.coords(
+                self._selection_box.tk_obj,
+                (self._selection_box.x_start * self._img_scale,
+                 self._selection_box.y_start * self._img_scale,
+                 self._selection_box.x_end * self._img_scale,
+                 self._selection_box.y_end * self._img_scale))
 
     def _left_release(self, event: Event) -> None:
         """Binding method for a left mouse button release event.
@@ -511,8 +515,8 @@ class ImageCanvas(ttk.Frame):
         """
 
         # Selection box is only considered if it exists and if it's big enough
-        if self._selection_box and \
-                self._selection_box.area * self._img_scale ** 2 > 25:
+        if (self._selection_box and
+            self._selection_box.area * self._img_scale ** 2 > 25):
             self._left_release_box(event)
         # Otherwise the event is treated as a sole click
         else:
@@ -535,27 +539,28 @@ class ImageCanvas(ttk.Frame):
         """
 
         # Do nothing if there's no image displayed
-        if self._image is not None:
+        if self._image is None:
+            return
 
-            # The coordinates of the click on the unscaled image
-            abs_x = self._canvas.canvasx(event.x) / self._img_scale
-            abs_y = self._canvas.canvasy(event.y) / self._img_scale
+        # The coordinates of the click on the unscaled image
+        abs_x = self._canvas.canvasx(event.x) / self._img_scale
+        abs_y = self._canvas.canvasy(event.y) / self._img_scale
 
-            # Update coordinates only if the click is inside the image
-            if abs_x < self._image.width and abs_y < self._image.height:
-                self._selection_box.x_end = abs_x
-                self._selection_box.y_end = abs_y
+        # Update coordinates only if the click is inside the image
+        if abs_x < self._image.width and abs_y < self._image.height:
+            self._selection_box.x_end = abs_x
+            self._selection_box.y_end = abs_y
 
-            self.log(f"Inversion box drawn from ("
-                     f"{self._selection_box.x_start}, "
-                     f"{self._selection_box.y_start}) to ("
-                     f"{self._selection_box.x_end}, "
-                     f"{self._selection_box.y_end})")
+        self.log(f"Inversion box drawn from ("
+                 f"{self._selection_box.x_start}, "
+                 f"{self._selection_box.y_start}) to ("
+                 f"{self._selection_box.x_end}, "
+                 f"{self._selection_box.y_end})")
 
-            # Inverting all the nuclei found inside the selection box
-            for nuc in self._nuclei:
-                if self._selection_box.is_inside(nuc.x_pos, nuc.y_pos):
-                    self._invert_nucleus(nuc)
+        # Inverting all the nuclei found inside the selection box
+        for nuc in self._nuclei:
+            if self._selection_box.is_inside(nuc.x_pos, nuc.y_pos):
+                self._invert_nucleus(nuc)
 
     def _left_release_nucleus(self, event: Event) -> None:
         """Upon left click, either adds a new nucleus or a fiber, or switches
@@ -566,28 +571,31 @@ class ImageCanvas(ttk.Frame):
         """
 
         # Do nothing if there's no image displayed
-        if self._image is not None:
+        if self._image is None:
+            return
 
-            # The coordinates of the click on the unscaled image
-            abs_x = self._canvas.canvasx(event.x) / self._img_scale
-            abs_y = self._canvas.canvasy(event.y) / self._img_scale
+        # Do nothing if the nuclei are not being displayed
+        if not self._settings.show_nuclei.get():
+            return
 
-            # Do something only if the event is inside the image
-            if abs_x < self._image.width and abs_y < self._image.height:
+        # The coordinates of the click on the unscaled image
+        abs_x = self._canvas.canvasx(event.x) / self._img_scale
+        abs_y = self._canvas.canvasy(event.y) / self._img_scale
 
-                # Do nothing if the nuclei are not being displayed
-                if self._settings.show_nuclei.get():
+        # Do something only if the event is inside the image
+        if abs_x >= self._image.width or abs_y >= self._image.height:
+            return
 
-                    # Trying to find a close nucleus
-                    nuc = self._find_closest_nucleus(abs_x, abs_y)
+        # Trying to find a close nucleus
+        nuc = self._find_closest_nucleus(abs_x, abs_y)
 
-                    # One close nucleus found, inverting it colour
-                    if nuc is not None:
-                        self._invert_nucleus(nuc)
+        # One close nucleus found, inverting it color
+        if nuc is not None:
+            self._invert_nucleus(nuc)
 
-                    # No close nucleus found, adding a new one
-                    else:
-                        self._add_nucleus(abs_x, abs_y)
+        # No close nucleus found, adding a new one
+        else:
+            self._add_nucleus(abs_x, abs_y)
 
     def _right_release(self, event: Event) -> None:
         """Binding method for a right mouse button release event.
@@ -601,8 +609,8 @@ class ImageCanvas(ttk.Frame):
         """
 
         # Selection box is only considered if it exists and if it's big enough
-        if self._selection_box and \
-                self._selection_box.area * self._img_scale ** 2 > 25:
+        if (self._selection_box and
+            self._selection_box.area * self._img_scale ** 2 > 25):
             self._right_release_box(event)
         # Otherwise the event is treated as a sole click
         else:
@@ -625,31 +633,36 @@ class ImageCanvas(ttk.Frame):
         """
 
         # Do nothing if there's no image displayed
-        if self._image is not None:
+        if self._image is None:
+            return
 
-            # The coordinates of the click on the unscaled image
-            abs_x = self._canvas.canvasx(event.x) / self._img_scale
-            abs_y = self._canvas.canvasy(event.y) / self._img_scale
+        # Do nothing if the nuclei are not being displayed
+        if not self._settings.show_nuclei.get():
+            return
 
-            # Update coordinates only if the click is inside the image
-            if abs_x < self._image.width and abs_y < self._image.height:
-                self._selection_box.x_end = abs_x
-                self._selection_box.y_end = abs_y
+        # The coordinates of the click on the unscaled image
+        abs_x = self._canvas.canvasx(event.x) / self._img_scale
+        abs_y = self._canvas.canvasy(event.y) / self._img_scale
 
-            self.log(f"Deletion box drawn from ("
-                     f"{self._selection_box.x_start}, "
-                     f"{self._selection_box.y_start}) to ("
-                     f"{self._selection_box.x_end}, "
-                     f"{self._selection_box.y_end})")
+        # Update coordinates only if the click is inside the image
+        if abs_x < self._image.width and abs_y < self._image.height:
+            self._selection_box.x_end = abs_x
+            self._selection_box.y_end = abs_y
 
-            to_delete = list()
-            # Detecting all the nuclei to delete
-            for nuc in self._nuclei:
-                if self._selection_box.is_inside(nuc.x_pos, nuc.y_pos):
-                    to_delete.append(nuc)
-            # Actually deleting the detected nuclei
-            for nuc in to_delete:
-                self._delete_nucleus(nuc)
+        self.log(f"Deletion box drawn from ("
+                 f"{self._selection_box.x_start}, "
+                 f"{self._selection_box.y_start}) to ("
+                 f"{self._selection_box.x_end}, "
+                 f"{self._selection_box.y_end})")
+
+        to_delete = list()
+        # Detecting all the nuclei to delete
+        for nuc in self._nuclei:
+            if self._selection_box.is_inside(nuc.x_pos, nuc.y_pos):
+                to_delete.append(nuc)
+        # Actually deleting the detected nuclei
+        for nuc in to_delete:
+            self._delete_nucleus(nuc)
 
     def _right_release_nucleus(self, event: Event) -> None:
         """Method called when the user releases the right mouse button but did
@@ -664,24 +677,27 @@ class ImageCanvas(ttk.Frame):
         """
 
         # Do nothing if there's no image displayed
-        if self._image is not None:
+        if self._image is None:
+            return
 
-            # The coordinates of the click on the unscaled image
-            abs_x = self._canvas.canvasx(event.x) / self._img_scale
-            abs_y = self._canvas.canvasy(event.y) / self._img_scale
+        # Do nothing if the nuclei are not being displayed
+        if not self._settings.show_nuclei.get():
+            return
 
-            # Do something only if the click is inside the image
-            if abs_x < self._image.width and abs_y < self._image.height:
+        # The coordinates of the click on the unscaled image
+        abs_x = self._canvas.canvasx(event.x) / self._img_scale
+        abs_y = self._canvas.canvasy(event.y) / self._img_scale
 
-                # Do nothing if the nuclei are not being displayed
-                if self._settings.show_nuclei.get():
+        # Do something only if the click is inside the image
+        if abs_x >= self._image.width or abs_y >= self._image.height:
+            return
 
-                    # Trying to find a close nucleus
-                    nuc = self._find_closest_nucleus(abs_x, abs_y)
+        # Trying to find a close nucleus
+        nuc = self._find_closest_nucleus(abs_x, abs_y)
 
-                    # One close nucleus found, deleting it
-                    if nuc is not None:
-                        self._delete_nucleus(nuc)
+        # One close nucleus found, deleting it
+        if nuc is not None:
+            self._delete_nucleus(nuc)
 
     def _find_closest_nucleus(self, x: float, y: float) -> Nucleus | None:
         """Searches for a close nucleus among the existing nuclei.
@@ -869,60 +885,61 @@ class ImageCanvas(ttk.Frame):
                 corner of the image.
         """
 
-        if self._image is not None:
+        if self._image is None:
+            return
 
-            # Linux mousewheel events do not hold information about the delta,
-            # so we need to set it manually
-            if delta is None:
-                if system() == "Linux":
-                    delta = 1 if event.num == 4 else -1
-                else:
-                    delta = int(event.delta / abs(event.delta))
+        # Linux mousewheel events do not hold information about the delta,
+        # so we need to set it manually
+        if delta is None:
+            if system() == "Linux":
+                delta = 1 if event.num == 4 else -1
+            else:
+                delta = int(event.delta / abs(event.delta))
 
-            scale = 1.0
-            # Setting the point where to zoom on the image
-            x_can = self._canvas.canvasx(event.x) if mouse else 0
-            y_can = self._canvas.canvasy(event.y) if mouse else 0
+        scale = 1.0
+        # Setting the point where to zoom on the image
+        x_can = self._canvas.canvasx(event.x) if mouse else 0
+        y_can = self._canvas.canvasy(event.y) if mouse else 0
 
-            # Zooming out
-            if delta < 0:
-                # Limit to 4 zooms out
-                if self._current_zoom < -4:
-                    return
+        # Zooming out
+        if delta < 0:
+            # Limit to 4 zooms out
+            if self._current_zoom < -4:
+                return
 
-                # Setting the new image and canvas scales
-                self._img_scale /= self._delta
-                self._can_scale /= self._delta
-                scale /= self._delta
-                self._current_zoom -= 1
+            # Setting the new image and canvas scales
+            self._img_scale /= self._delta
+            self._can_scale /= self._delta
+            scale /= self._delta
+            self._current_zoom -= 1
 
-                # Scrolling the canvas to keep the mouse on the same point
-                self._canvas.xview_scroll(int((1 / self._delta - 1) * x_can),
-                                          "units")
-                self._canvas.yview_scroll(int((1 / self._delta - 1) * y_can),
-                                          "units")
+            # Scrolling the canvas to keep the mouse on the same point
+            self._canvas.xview_scroll(int((1 / self._delta - 1) * x_can),
+                                      "units")
+            self._canvas.yview_scroll(int((1 / self._delta - 1) * y_can),
+                                      "units")
 
-            # Zooming in
-            elif delta > 0:
-                # Limit to 4 zooms in
-                if self._current_zoom > 4:
-                    return
+        # Zooming in
+        elif delta > 0:
+            # Limit to 4 zooms in
+            if self._current_zoom > 4:
+                return
 
-                # Setting the new image and canvas scales
-                self._img_scale *= self._delta
-                self._can_scale *= self._delta
-                scale *= self._delta
-                self._current_zoom += 1
+            # Setting the new image and canvas scales
+            self._img_scale *= self._delta
+            self._can_scale *= self._delta
+            scale *= self._delta
+            self._current_zoom += 1
 
-            # Rescaling the canvas
-            self._canvas.scale('all', 0, 0, scale, scale)
-            # Updating the image
-            self.show_image()
+        # Rescaling the canvas
+        self._canvas.scale('all', 0, 0, scale, scale)
+        # Updating the image
+        self.show_image()
 
-            # Scrolling the canvas to keep the mouse on the same point in case
-            # we were zooming in and the image is bigger than the canvas
-            if delta > 0 and self._current_zoom > 0:
-                self._canvas.xview_scroll(int((self._delta - 1) * x_can),
-                                          "units")
-                self._canvas.yview_scroll(int((self._delta - 1) * y_can),
-                                          "units")
+        # Scrolling the canvas to keep the mouse on the same point in case
+        # we were zooming in and the image is bigger than the canvas
+        if delta > 0 and self._current_zoom > 0:
+            self._canvas.xview_scroll(int((self._delta - 1) * x_can),
+                                      "units")
+            self._canvas.yview_scroll(int((self._delta - 1) * y_can),
+                                      "units")
